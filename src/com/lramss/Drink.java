c1e112674bfac8bd5517e0ff20667324410a70ec
Loghan <loghan.r@gmail.com>
2019-04-09 01:00:37 +0200

    Add drink maker message and customer message

--- src/com/lramss/Drink.java
+++ src/com/lramss/Drink.java
@@ -1,21 +1,46 @@
 package com.lramss;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Drink {
     private String drinkType;
     private int nbSugar;
     private int stick;
+    private final String UNKNOWN_DRINK = "unknown drink";
+    private Map<String, String> drinks = new HashMap<String, String>()
+    {
+        {
+            put("T", "Tea");
+            put("C", "Coffee");
+            put("H", "Chocolate");
+        }
+    };
 
-    Drink(String boisonType){
-        this.drinkType = boisonType;
+    public Drink(String drinkType){
+        this.drinkType = drinkType;
     }
 
-    Drink(String boisonType, int nbSugar){
+    public Drink(String boisonType, int nbSugar){
         this.drinkType = boisonType;
         this.nbSugar = nbSugar;
         this.stick = (nbSugar > 0) ? 1 : 0;
     }
 
-    @Override
-    public String toString() {
+    private String defineDrink(String code){
+        return drinks.getOrDefault(code,"unknown drink");
+    }
+
+    public String toStringForCustomer() {
+        String msg;
+        if (UNKNOWN_DRINK.equals(defineDrink(drinkType)))
+            msg = "Drink maker will don't makes your drink. (" + UNKNOWN_DRINK + ")";
+        else
+            msg = "Drink maker makes 1 " + defineDrink(drinkType) + " with " + nbSugar + " sugar and " + stick + " stick.";
+        return msg;
+    }
+
+    public String toStringForDrinkMaker() {
         return drinkType + ':' + nbSugar + ':' + stick;
     }
 }
--- src/com/lramss/Main.java
+++ src/com/lramss/Main.java
@@ -3,7 +3,7 @@ package com.lramss;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(new Drink("T",2));
-	System.out.println(new Drink("C"));
+	System.out.println(new Drink("A",2).toStringForCustomer());
+	System.out.println(new Drink("C").toStringForCustomer());
     }
 }
